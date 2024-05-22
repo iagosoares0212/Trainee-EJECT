ebc311ed933547dc91641e33883710220a4ec2fb
iagosoares0212 <57151958+iagosoares0212@users.noreply.github.com>
2024-05-22 15:01:52 -0300

    iniciando - Semana3

---
+++ Semana3/Seção2/script.js
@@ -0,0 +1,43 @@
+var nome = 'André'
+var idade = 28;
+var possuiFaculdade = true
+
+console.log(nome, idade, possuiFaculdade, nome)
+
+var preco = 25;
+var totalComprado = 5;
+var TotalPreco = totalComprado * preco
+
+console.log(TotalPreco)
+
+var sobrenome = 'Rafael', 
+    cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefini);
+ 
+var comida;
+console.log(comida);
+var comida = 'Pizza';
+console.log(comida);
+
+// const time = 'Vasco';
+// console.log(time);
+
+// Exercício da aula
+var nome = 'Iago';
+var idade = 23;
+var comidaFavorita;
+comidaFavorita = 'Sushi';
+var eu,
+    tu,
+    ele,
+    nos,
+    vos;
+    
+console.log(nome, idade, comidaFavorita)
+
