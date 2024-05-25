291e75a9c02513c326bf199f68f52281472cbc6d
iagosoares0212 <iagosoares55@gmail.com>
2024-05-25 13:40:58 -0300

    objetos

--- Semana3/Seção2/Objetos/script.js
+++ Semana3/Seção2/Objetos/script.js
@@ -50,7 +50,7 @@ Math.PI; // 3.14
 Math.random(); // número aleatório
 
 var pi = Math.PI;
-console.log(pi); // 3.14
+// console.log(pi);  3.14
 
 // Math é um objeto nativo de JavaScript. Já percebeu que console é um objeto e log() um método?
 
@@ -60,7 +60,7 @@ console.log(pi); // 3.14
 var carro = {};
 var pessoa = {};
 
-console.log(typeof carro); // 'object'
+// console.log(typeof carro);  'object'
 
 // DOT NOTATION GET
 // Acesse propriedades de um objeto utilizando o ponto .
@@ -73,4 +73,103 @@ var menu = {
 
 var bg = menu.backgroundColor; // '#84E'
 
+// DOT NOTATION SET
+// Substitua o valor de uma propriedade utilizando . e o = após o nome da mesma.
 
+var menu = {
+  width: 800,
+  height: 50,
+  backgroundColor: '#84E',
+}
+
+menu.backgroundColor = '#000';
+console.log(menu.backgroundColor); // '#000'
+
+// ADICIONAR PROPRIEDADES E MÉTODOS
+// Basta adicionar um novo nome e definir o valor.
+
+var menu = {
+  width: 800,
+}
+
+menu.height = 50;
+menu.position = 'fixed';
+
+// PALAVRA-CHAVE THIS
+// this irá fazer uma referência ao próprio objeto.
+
+var height = 120;
+var menu = {
+  width: 800,
+  height: 50,
+  metadeHeight() {
+    return this.height / 2;
+  }
+}
+
+menu.metadeHeight(); // 25
+// sem o this, seria 60
+
+// PROTÓTIPO E HERANÇA
+// O objeto herda propriedades e métodos do objeto que foi utilizado para criar o mesmo.
+
+var menu = {
+  width: 800,
+}
+
+menu.hasOwnProperty('width') // true
+menu.hasOwnProperty('height') // false
+
+//Exercícios
+
+// 1)Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+var dados = {
+  nome: 'Iago',
+  sobrenome: 'Soares',
+}
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+dados.nomeCompleto = function(){
+  return this.nome + " " + this.sobrenome
+}
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+carro.preco = 3000;
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+var cachorrin = {
+  raca: 'labrador',
+  cor: 'preto',
+  idade: 10,
+  late() {
+    r = prompt('O cachorro viu o homem ?')
+    if (r === 'sim'){
+      return 'O cachorro late'
+    } else {
+      return 'O Cachorro não late'
+    }
+  },
+}
+
+// correção
+
+var cachorro = {
+  raca: 'labrador',
+  cor: 'preto',
+  idade: 10,
+  latir(pessoa) {
+    if (pessoa === 'homem'){
+      return 'Latir'
+    } else {
+      return 'Nada'
+    }
+  },
+}
