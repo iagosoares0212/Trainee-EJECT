95f12e393e3413bf4a397f44af29e4cfb344f14d
iagosoares0212 <iagosoares55@gmail.com>
2024-06-13 15:30:45 -0300

    Desenvolvido até animação ao scroll

--- Semana3/Seção3 - Dom para iniciantes/Animação ao Scroll/script.js
+++ Semana3/Seção3 - Dom para iniciantes/Animação ao Scroll/script.js
@@ -40,3 +40,38 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start' ,
+    });
+
+    // forma alternativa 
+    // const topo = (section.offsetTop)
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth'
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection)
+  })
+}
+initScrollSuave();
+
+const sections = document.querySelectorAll('.js-scroll');
+
+function animaScroll() {
+  console.log('Sim')
+}
+
+window.addEventListener('scroll', animaScroll)
