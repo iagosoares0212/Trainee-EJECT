6ff1e121c016814608440b9ee73d8debf4410971
iagosoares0212 <iagosoares55@gmail.com>
2024-09-03 10:09:11 -0300

    .

---
+++ Semana3/Seção5 - Efeitos no Dom.js/dataset/script.js
@@ -0,0 +1,75 @@
+// HTMLElement
+// Todo elemento HTML do DOM herda propriedades e métodos do construtor HTMLElement.
+
+// const h1 = document.querySelector('h1');
+// Object.prototype.toString.call(h1); // [object HTMLHeadingElement]
+// // HTMLHeadingElement > HTMLElement > Element > Node > EventTarget > Object
+
+const div = document.querySelector('div');
+const div1 = document.querySelector('[data-cor]');
+div.dataset.height = 1000;
+
+delete div.dataset.height;
+
+div.dataset.totalHeight = 2000;
+
+console.log(div.dataset)
+
+// Dataset
+
+// dataset é uma propriedade de HTMLElement, essa propriedade é um objeto do tipo DOMStringMap. Dentro desse objeto existe uma coleção de chave / valor, com todos os atributos do elemento html que começarem com data-.
+
+// Data Atributes
+
+// Os atributos e valores que começarem com data- poderão ser utilizados como forma de configuração de plugins e interações DOM / JS.
+
+// <div data-anima="left" data-tempo="1000">Div 1</div>
+// <div data-anima="right" data-tempo="2000">Div 2</div>
+
+// const divs = document.querySelectorAll('[data-anima]');
+// divs.forEach((div) => {
+//   div.classList.add(div.dataset.anima);
+// });
+
+// // adiciona em cada div
+// // uma classe com o mesmo nome
+// // que o valor de data
+
+// Data vs Class
+
+// A vantagem de se utilizar data atributes é que torna mais fácil evitarmos conflitos com estilos do CSS. Além de deixar a estrutura da tag mais organizada.
+
+{/* <div data-anima="left" data-tempo="1000">Div 1</div>
+<div class="anima-left tempo-1000">Div 2</div> */}
+
+// Nomenclatura
+
+// Por padrão o JavaScript não aceita - (traço) como caracter válido para nomear propriedades. Então qualquer traço será removido e a letra seguinte transformada em maiúscula.
+
+{/* <div data-anima-scroll="left">Div 1</div>
+
+const div = document.querySelector('[data-anima-scroll]');
+div.dataset;
+// {animaScroll: 'left'}
+div.dataset.animaScroll; // left
+div.dataset.tempoTotal = 1000;
+// Na div vira data-tempo-total="1000"
+
+delete div.dataset.animaScroll; // remove o atributo */}
+
+// Exercícios
+
+// Adicione um atributo data-anime="show-down" e
+// data-anime="show-right" a todos as section's
+// com descricão dos animais.
+
+// Utilizando estes atributos, adicione a classe
+// show-down ou show-right a sua respectiva section
+// assim que a mesma aparecer na tela (animacao tab)
+
+// No CSS faça com que show-down anime de cima para baixo
+// e show-right continue com a mesma animação da esquerda
+// para a direita
+
+// Substitua todas as classes js- por data atributes.
+
